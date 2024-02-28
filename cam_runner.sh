cc12485f82d9a38c2264a654023a2b730d27d07f
ebs27 <55114341+ebs27@users.noreply.github.com>
2024-02-28 11:32:08 -0500

    main and runner initializes/closes pylon session with user input on FPS and no of frames

--- cam_runner.sh
+++ cam_runner.sh
@@ -1,4 +1,6 @@
 echo "Running main.exe"
 # ------------------Call Code---------------
-# Change YourNameHere to desired input argument
-./src/main YourNameHere
+# Call main.exe and input arguments
+# (1) FPS
+# (2) Total No. of Frames (runtime (s)= no. of frames * FPS)
+./src/main 25.5 100
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,40 @@
-#include <iostream>
+// ----------------------------HEADERS------------------------------------------------------
 
-// Generate a Hello, World! Message. 
-// Argv[1] is a name that user can enter as an argument 
-// to main function to test functionality
+#include <pylon/PylonIncludes.h>
+#include <pylon/BaslerUniversalInstantCamera.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) 
+using namespace Pylon;
+using namespace std;
+using namespace Basler_UniversalCameraParams;
+
+// -----------------------------BEGIN MAIN -------------------------------------------------
+int main( int argc, char** argv)
 {
-    std::cout << "HELLO, " << argv[1] << "!"<< std::endl;
+    //--------------------------ASSIGN ARGUMENTS TO VARIABLES---------------------------
+    
+    double FPS = atof(argv[1]);         // Frames per second, pulled from first input argument
+    int NumFrames = atof(argv[2]);      // Number of frames (FPS * Runtime(s)), pulled from second input argument
+
+    cout << "FPS: " << FPS << ", No. of Frames: " << NumFrames << "\n"; // print input values to the Command line
+
+
+    //------------------------INIT PYLON--------------------------------------------------
+    // Exit code of sample application
+    int exitCode = 0;
+
+    // Before using pylon methods, initialize pylon runtime
+    PylonInitialize();
+
+
+    // -----------------------EXIT PYLON---------------------------------------------------
+    cerr <<endl << "Press enter to exit." << endl;
+    while (cin.get() != '\n');
+    
+    // Release pylon resources
+    PylonTerminate();
+
+    return exitCode;
+
 }
