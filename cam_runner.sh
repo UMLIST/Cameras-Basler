34c8129bf0cbaf9437ea4ad020ebb55148e037a2
ebs27 <55114341+ebs27@users.noreply.github.com>
2024-02-28 12:10:35 -0500

    save mp4 video at set frame rate for number of frames

--- cam_runner.sh
+++ cam_runner.sh
@@ -3,4 +3,4 @@ echo "Running main.exe"
 # Call main.exe and input arguments
 # (1) FPS
 # (2) Total No. of Frames (runtime (s)= no. of frames * FPS)
-./src/main 25.5 100
+./src/main 25.0 100
--- src/main.cpp
+++ src/main.cpp
@@ -17,24 +17,113 @@ int main( int argc, char** argv)
     double FPS = atof(argv[1]);         // Frames per second, pulled from first input argument
     int NumFrames = atof(argv[2]);      // Number of frames (FPS * Runtime(s)), pulled from second input argument
 
-    cout << "FPS: " << FPS << ", No. of Frames: " << NumFrames << "\n"; // print input values to the Command line
-
-
+    
     //------------------------INIT PYLON--------------------------------------------------
     // Exit code of sample application
     int exitCode = 0;
 
     // Before using pylon methods, initialize pylon runtime
-    PylonInitialize();
+    Pylon::PylonAutoInitTerm autoInitTerm; //PylonInitialize() will be called now
 
+    CBaslerUniversalInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice() );
+    // create first camera device
+    // Use CBaslerUniversalInstantCamera or else it throws error
+    
+    //----------------------SETTINGS MANAGEMENT---------------------------------------------
+    camera.Open();
+    
+    // open camera for settings parameters
+    camera.AcquisitionFrameRate.SetValue(FPS);
+    double FPS_out = camera.AcquisitionFrameRate.GetValue();
+
+    camera.AcquisitionMode.SetValue(AcquisitionMode_Continuous);
+    // set acquisition mode to continuous
+
+    camera.TriggerSelector.SetValue(TriggerSelector_FrameStart);
+    camera.TriggerMode.SetValue(TriggerMode_Off);
+    // set trigger to frame start
+
+
+    // ----------VIDEO MANAGEMENT----------
+    // Create a video writer object.
+    CVideoWriter videoWriter;
+    // The parameter MaxNumBuffer can be used to control the count of buffers
+    // allocated for grabbing. The default value of this parameter is 10.
+    camera.MaxNumBuffer = 10;
+
+    // Get the required camera settings.
+    CIntegerParameter width( camera.GetNodeMap(), "Width" );
+    CIntegerParameter height( camera.GetNodeMap(), "Height" );
+    CEnumParameter pixelFormat( camera.GetNodeMap(), "PixelFormat" );
+
+    // Optional: Depending on your camera or computer, you may not be able to save
+    // a video without losing frames. Therefore, we limit the resolution:
+    width.TrySetValue( 1920, IntegerValueCorrection_Nearest );
+    height.TrySetValue( 1080, IntegerValueCorrection_Nearest );
+
+    // Map the pixelType
+    CPixelTypeMapper pixelTypeMapper( &pixelFormat );
+    EPixelType pixelType = pixelTypeMapper.GetPylonPixelTypeFromNodeValue( pixelFormat.GetIntValue() );
+
+    // Set parameters before opening the video writer.
+    videoWriter.SetParameter(
+    (uint32_t) width.GetValue(),
+            (uint32_t) height.GetValue(),
+            pixelType,
+            FPS_out,
+            90 );
+
+    //---PRINT TO CLI-----------------------------------------------------------------------
+    cout << "FPS: " << FPS_out << endl;
+    cout << "No. of Frames: " << NumFrames << endl; 
+    cout << "Camera Model: " << camera.GetDeviceInfo().GetModelName() << endl; // get the cameras model name and print to terminal
+
+    camera.Close();
+
+    //-------------------START CAM/VIDEO--------------
+    // Open the video writer.
+    videoWriter.Open( "_TestVideo.mp4" );
+
+    // Start the grabbing of c_countOfImagesToGrab images.
+    // The camera device is parameterized with a default configuration which
+    // sets up free-running continuous acquisition.
+    camera.StartGrabbing( NumFrames );
+
+    CGrabResultPtr ptrGrabResult;
+
+    while (camera.IsGrabbing())
+        {
+            // Wait for an image and then retrieve it. A timeout of 5000 ms is used.
+            camera.RetrieveResult( 5000, ptrGrabResult, TimeoutHandling_ThrowException );
+
+            // Image grabbed successfully?
+            if (ptrGrabResult->GrabSucceeded())
+            {
+                // Access the image data.
+                cout << "SizeX: " << ptrGrabResult->GetWidth() << endl;
+                cout << "SizeY: " << ptrGrabResult->GetHeight() << endl;
+                const uint8_t* pImageBuffer = (uint8_t*) ptrGrabResult->GetBuffer();
+                cout << "Gray value of first pixel: " << (uint32_t) pImageBuffer[0] << endl << endl;
+            }
+
+            else
+            {
+                cout << "Error: " << std::hex << ptrGrabResult->GetErrorCode() << std::dec << " " << ptrGrabResult->GetErrorDescription() << endl;
+            }
+
+        videoWriter.Add( ptrGrabResult );
+
+            // If images are skipped, writing video frames takes too much processing time.
+            cout << "Images Skipped = " << ptrGrabResult->GetNumberOfSkippedImages() << boolalpha
+            << "; Image has been converted = " << !videoWriter.CanAddWithoutConversion( ptrGrabResult )
+            << endl;
+        }
 
     // -----------------------EXIT PYLON---------------------------------------------------
     cerr <<endl << "Press enter to exit." << endl;
     while (cin.get() != '\n');
     
-    // Release pylon resources
-    PylonTerminate();
 
     return exitCode;
 
-}
+} //AutoInitTerm's descructor calls PylonTerminate() now
